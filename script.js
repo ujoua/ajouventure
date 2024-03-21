43bfc17c877a7ad2c751aeda22c8264e8ed7d3db
KimSumin <ajou456@ajou.ac.kr>
2024-03-21 15:01:12 +0900

    Customize the map lat/lng and zoom level

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ async function initMap() {
     const { Map } = await google.maps.importLibrary("maps");
 
     map = new Map(document.getElementById("map"), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8,
+        center: { lat: 37.282391, lng: 127.043502 },
+        zoom: 18,
     });
 }
 
