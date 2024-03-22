9f1b1fa42f488c08a6c2967c661d0e4f856fe529
KimSumin <ajou456@ajou.ac.kr>
2024-03-22 23:54:24 +0900

    Implement custom map markers

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 let map;
 
 async function initMap() {
-    const { Map } = await google.maps.importLibrary("maps");
+    const myLatLng = { lat: 37.282391, lng: 127.043502 };
 
+    const { Map } = await google.maps.importLibrary("maps");
     map = new Map(document.getElementById("map"), {
-        center: { lat: 37.282391, lng: 127.043502 },
+        center: myLatLng,
         zoom: 18,
+    });
+
+    new google.maps.Marker({
+        map,
+        position: myLatLng,
+        title: "Hello World!",
+        icon: {
+            url: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
+            scaledSize: new google.maps.Size(38, 31),
+        },
     });
 }
 
